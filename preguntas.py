d96dfa78c1ef3ad0e2a6c18978b4dd4707aa7024
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-12-02 00:10:10 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -47,23 +47,24 @@ def pregunta_02():
     Complete el código presentado a continuación.
     """
 
-    # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+   # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
+    df = pd.read_csv("gm_2008_region.csv")
 
     # Imprima las dimensiones del DataFrame
-    print(____.____)
+    print(df.shape)
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    print(____)
+    print(round(np.corrcoef(df["life"],df["fertility"])[0,1],4))
 
     # Imprima la media de la columna `life` con 4 decimales.
-    print(____)
+    print(round(np.mean(df["life"]),4))
 
     # Imprima el tipo de dato de la columna `fertility`.
-    print(____)
+    print(type(df["fertility"]))
 
     # Imprima la correlación entre las columnas `GDP` y `life` con 4 decimales.
-    print(____)
+    print(round(np.corrcoef(df["GDP"],df["life"])[0,1],4))
+
 
 
 def pregunta_03():
